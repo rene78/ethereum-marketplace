4e17cc0652fd2226bcd992f45582ccccad8ebd40
Rene <36115705+rene78@users.noreply.github.com>
2019-09-03 20:46:31 +0200

    Updates

---
+++ precache-manifest.d13e43be8970a15e7fcc2bae2085181d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42e5a1b95d82815e13c3",
+    "url": "/ethereum-marketplace/static/js/runtime~main.42e5a1b9.js"
+  },
+  {
+    "revision": "8913b38ef944e42ce0f5",
+    "url": "/ethereum-marketplace/static/js/main.8913b38e.chunk.js"
+  },
+  {
+    "revision": "1fe7a3ef683c633f2aa6",
+    "url": "/ethereum-marketplace/static/js/1.1fe7a3ef.chunk.js"
+  },
+  {
+    "revision": "8913b38ef944e42ce0f5",
+    "url": "/ethereum-marketplace/static/css/main.f5b227ca.chunk.css"
+  },
+  {
+    "revision": "1fe7a3ef683c633f2aa6",
+    "url": "/ethereum-marketplace/static/css/1.8fa73d37.chunk.css"
+  },
+  {
+    "revision": "c2bde3c71f4eab8973a67ae2f01c7e28",
+    "url": "/ethereum-marketplace/index.html"
+  }
+];
